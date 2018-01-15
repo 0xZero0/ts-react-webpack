cbfdb5fe4dff25c31b69915b4f288601eec68f7a
venpear <venpearzh@gmail.com>
2018-01-16 00:08:16 +0800

    webpack react typescript init

---
+++ server.js
@@ -0,0 +1,12 @@
+var config = require("./webpack.config.js");
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+
+config.entry.app.unshift("webpack-dev-server/client?http://localhost:8080/");
+
+var compiler = webpack(config);
+var server = new WebpackDevServer(compiler, {
+    contentBase: 'build/',
+    publicPath: "/assets/"
+});
+server.listen(8080)
